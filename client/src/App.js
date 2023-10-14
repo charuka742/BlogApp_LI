984385bdfa11db555a37298d62189ffec553d4f3
Disanayaka AAC <charair742@gmail.com>
2023-10-14 15:14:12 +0530

    initial Commit for the client side

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import NotFoundPage from './pages/NotFoundPage.';
+import NavBar from './NavBar';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+          <NavBar/>
+          <div id="page-body">
+            <Routes>
+            <Route path="/" element={<HomePage/>}/>
+              <Route path="/about" element={<AboutPage/>}/>
+              <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+              <Route path="/articles" element={<ArticlesListPage/>}/>
+              <Route path="*" element={<NotFoundPage/>}/>  
+            </Routes>
+          </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
