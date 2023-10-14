d7eb8afc49e6062612d5d76e365764076c1bff54
Disanayaka AAC <charair742@gmail.com>
2023-10-14 23:38:28 +0530

    server works 200ok

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 
-let ArticleInfo = [{
+let articleInfo = [{
     name:'learn-react',
-    upvote: 0
+    upvotes: 0,
+    comments:[],
 },
 {
     name:'learn-node',
-    upvote: 0
+    upvotes: 0,
+    comments:[],
 },
 {
     name:'mongodb',
-    upvote: 0
+    upvotes: 0,
+    comments:[],
 }]
 
 const app = express();
@@ -19,16 +22,31 @@ app.use(express.json());
 
 
 app.put('/api/articles/:name/upvote', (req, res) => {
-    
+    const {name} = req.params;
+    const article = articleInfo.find(a => a.name === name);
+
+    if(article){
+        article.upvotes = article.upvotes + 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes.`);
+    }else{
+        res.send('That article doesn\'t exist.');
+    }
+
 });
 
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const {postedBy, text} = req.body;
 
+    const article = articleInfo.find(a => a.name === name);
+    if(article){
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+    }else{
+        res.send('That article doesn\'t exist.');
+    }
 
-
-
-
-
-
+})
 
 
 // app.get('/hello/:name', (req, res) => {
