bdb994d44d578d72774cdc44a3604507946e1a38
Disanayaka AAC <charair742@gmail.com>
2023-10-18 14:51:36 +0530

    Rewrited the endpoit for adding comments

--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,26 @@
 import express from "express";
-import {MongoClient} from 'mongodb';  
+import {db, connectToDb} from './db.js';
 const app = express();
 app.use(express.json());
 
 
 app.get('/api/articles/:name', async(req, res) => {
     const { name } = req.params;
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
-
+    
     const article = await db.collection('articles').findOne({ name });
 
     if(article){
         res.json(article);
     } else {
         res.sendStatus(404);
-    }
-    
+    } 
 
 })
 
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const {name} = req.params;
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
+   
     await db.collection('articles').updateOne({ name },{
         $inc: {upvotes :1},
     });
@@ -36,7 +28,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
     const article = await db.collection('articles').findOne({name});
 
     if(article){
-        article.upvotes = article.upvotes + 1;
+        // article.upvotes = article.upvotes + 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes.`);
     }else{
         res.send('That article doesn\'t exist.');
@@ -45,20 +37,29 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 });
 
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
     const {name} = req.params;
     const {postedBy, text} = req.body;
+    
+    await db.collection('articles').updateOne({ name },{
+        $push: {comments:{postedBy, text}},
+    });
 
-    const article = articleInfo.find(a => a.name === name);
+    const article = await db.collection('articles').findOne({name});
+
     if(article){
-        article.comments.push({postedBy, text});
         res.send(article.comments);
     }else{
         res.send('That article doesn\'t exist.');
     }
+});
 
-})
 
-app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
+connectToDb(() =>{
+    console.log("successfully connected to the database.!!");
+
+    app.listen(8000, () => {
+        console.log("Server is listening on port 8000");
+    });
+
 });
