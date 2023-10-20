9cc42718058a5d9bf3b2adfab5693cdff84336bc
Disanayaka AAC <charair742@gmail.com>
2023-10-20 12:39:28 +0530

    protecting endpoints using firebase auth tokens

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,41 @@
+import fs from 'fs';
+import admin from 'firebase-admin';
 import express from "express";
 import {db, connectToDb} from './db.js';
+
+const credentials = JSON.parse(
+    fs.readFileSync('../credentials.json')
+);
+admin.initializeApp({
+    credential: admin.cert(credentials),
+})
+
 const app = express();
 app.use(express.json());
+
+app.use(async (req, res, next) => {
+    const {authToken} = req.headers;
+
+    if(authToken) {
+        try{
+            req.user = await admin.auth().verifyAuthToken(authToken);
+        }catch (e) {
+            res.sendStatus(400);
+        }
+    }
+    next();
+})
 
 
 app.get('/api/articles/:name', async(req, res) => {
     const { name } = req.params;
-    
+    const { uid } = req.user;
+
     const article = await db.collection('articles').findOne({ name });
 
     if(article){
+        const upvotesId = article.upvotesId || [];
+        article.canUpvote = uid && !upvotesId.include(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
