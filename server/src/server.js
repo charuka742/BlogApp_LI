3157d092c348fdf154472372d7e2dbb918b57b4e
Disanayaka AAC <charair742@gmail.com>
2023-10-25 07:39:37 +0530

    built the frontend and ready to deploy

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,15 @@
 import fs from 'fs';
 import admin from 'firebase-admin';
+import path from 'path';
 import express from "express";
 import {db, connectToDb} from './db.js';
+import 'dotenv/config.js';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 const credentials = JSON.parse(
     fs.readFileSync('./credentials.json')
@@ -13,7 +21,11 @@ admin.initializeApp({
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'../build')));
 
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname,'../build/index.html'));
+})
 
 app.use(async (req, res, next) => {
     const {authToken} = req.headers;
@@ -105,11 +117,13 @@ app.post('/api/articles/:name/comments', async (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 8000;
+
 connectToDb(() =>{
     console.log("successfully connected to the database.!!");
 
-    app.listen(8000, () => {
-        console.log("Server is listening on port 8000");
+    app.listen(PORT, () => {
+        console.log("Server is listening on port " + PORT);
     });
 
 });
